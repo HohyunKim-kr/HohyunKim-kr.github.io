6295cab1aa98487f94ddb989ac47604fc42d214b
Hohyun.Kim <hohyun1022@naver.com>
2023-09-12 22:38:43 +0900

    feat: add filtering Js

--- Hohyun's Portfolio/src/projects.js
+++ Hohyun's Portfolio/src/projects.js
@@ -1,1 +1,42 @@
 "use strict";
+
+//프로젝트 필터링 관련 로직 처리
+
+const categories = document.querySelector(".categories");
+const projects = document.querySelectorAll(".project");
+const projectsContainer = document.querySelector(".projects");
+
+categories.addEventListener("click", (e) => {
+  const filter = e.target.dataset.category;
+  if (filter == null) {
+    return;
+  }
+
+  // Active 메뉴 재설정
+  handleActiveSelection(e.target);
+
+  // 프로젝트를 필터링
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  const active = document.querySelector(".category--selected");
+  active.classList.remove("category--selected");
+  target.classList.add("category--selected");
+}
+
+function filterProjects(filter) {
+  projectsContainer.classList.add("anim-out");
+  projects.forEach((project) => {
+    console.log(project);
+    if (filter === "all" || filter === project.dataset.type) {
+      project.style.display = "block";
+    } else {
+      project.style.display = "none";
+    }
+  });
+  projectsContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projectsContainer.classList.remove("anim-out");
+  }, 250);
+}
