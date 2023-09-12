7f214f948580a9283cbadc0fcef30ffeed3d50d8
Hohyun.Kim <hohyun1022@naver.com>
2023-09-12 19:13:59 +0900

    feat: arrowUp JS

--- Hohyun's Portfolio/src/main.js
+++ Hohyun's Portfolio/src/main.js
@@ -1,12 +1,30 @@
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector(".header");
-const headerRect = header.getBoundingClientRect();
-const headerHeight = headerRect.height;
+const headerHeight = header.offsetHeight;
 
 document.addEventListener("scroll", () => {
   if (window.scrollY > headerHeight) {
     header.classList.add("header--dark");
   } else {
     header.classList.remove("header--dark");
+  }
+});
+
+// Home 섹션을 아래로 스크롤 시 투명하게 처리
+const home = document.querySelector(".home__container");
+const homeHeight = home.offsetHeight;
+
+document.addEventListener("scroll", () => {
+  console.log(1 - window.scrollY / homeHeight);
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Arrow up 버튼을 아래로 스크롤 시 투명하게 처리함
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
   }
 });
